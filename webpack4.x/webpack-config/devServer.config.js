4f64fd12b2e6c96009ad34601754ebf45a740810
djkloop <1044032256@qq.com>
2018-04-07 16:06:33 +0800

    docs(README.md): :rocket: README.md update webpack-dev-server openPage options

--- webpack4.x/webpack-config/devServer.config.js
+++ webpack4.x/webpack-config/devServer.config.js
@@ -1,6 +1,7 @@
 var serverOptions = require('./common-var')
 module.exports = {
   contentBase: './build/',
+  openPage: "app_index.html", // 指定打开首页
   host: serverOptions.devOptions.server.host,
   port: serverOptions.devOptions.server.port,
   inline: serverOptions.devOptions.server.inline,
